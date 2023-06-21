200230076da9c703c797b909b68073939543c76f
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2023-06-21 12:31:59 +0530

    Adds CW alarms for cpu, memory and disk usage

--- sysops-admin/data.tf
+++ sysops-admin/data.tf
@@ -1,6 +1,6 @@
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
-data "aws_region" "current" {}
+# data "aws_region" "current" {}
 
 data "terraform_remote_state" "target_infra" {
   backend = "s3"
